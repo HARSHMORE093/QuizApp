46e5298a26c29bf74ce10593763070919383d85e
HARSHMORE093 <moreharsh093@gmail.com>
2021-10-11 16:59:35 +0530

    Share botton in gameWonFragment

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,10 +16,9 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -36,6 +35,28 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_gameWonFragment_to_gameFragment)
         )
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu,menu)
+    }
+    private fun getShareIntent():Intent{
+        val shareIntent=Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT,getString(R.string.share_success_text))
+        return shareIntent
+    }
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share->shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
